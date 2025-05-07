4b0571587d9dead8504691bf2ab52c867c288e5b
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-05-07 19:05:34 +0200

    establishing LP Blog unit, adding elements into project

--- src/app/components/landingPage/blog/landingPageBlog.tsx
+++ src/app/components/landingPage/blog/landingPageBlog.tsx
@@ -1,17 +1,17 @@
 import getAllPosts from "@/integrations/wordpress/getAllPosts"
+import styles from './landingPageBlog.module.scss';
 
 export default async function LandingPageBlog() {
 
     /* const posts = await getAllPosts(); */
 
 
-    const unitAppearance:string = 'w-1/3 h-[80%] border border-black hover:bg-gray-400 hover:backdrop-filter hover:backdrop-blur-sm hover:bg-opacity-10';
+    const unitAppearance:string = 'w-1/3 h-[80%] border border-black bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10';
 
-    return (
-        
-        <div className='w-screen h-screen flex items-center justify-evenly'>
+    return (       
+        <div className='w-screen h-screen flex items-center justify-evenly relative z-[10]'>
             <div className={unitAppearance}></div>
-            <div className={unitAppearance}></div>
+            <div className={unitAppearance}></div>                                                
         </div>
     )
-}
+}        
--- src/app/components/landingPage/video/landingPageVideo.tsx
+++ src/app/components/landingPage/video/landingPageVideo.tsx
@@ -27,7 +27,7 @@ export default function LandingPageVideo() {
 
     return(
         <div className="w-screen h-screen relative">
-            <Image alt="animacja tytułowa" className="w-full h-full object-cover fixed z-[-1]" src={video}/>
+            <Image alt="animacja tytułowa" className="w-full h-full object-cover fixed z-[-2]" src={video}/>
             <div className={classNames({["fixed bottom-0 text-white"]: true, [styles.hide]: !titleVisible, [styles.show]: titleVisible})}>
                 <div className={classNames({["relative border-t-2 border-white h-60 w-[84vw]"]:true,/*  [styles.hide]:!titleVisible */})}>
                     <h1 className="text-[15vw] absolute bottom-0 left-[-1vw] leading-[10vw]">DOSŁOWNIE</h1> 
