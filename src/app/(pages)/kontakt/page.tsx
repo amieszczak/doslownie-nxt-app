4859de8b5c7d9ab67e22e27b9011ed77f4bc2f5d
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-05-07 20:14:20 +0200

    adding dummy data; subpageEntryComponent

--- src/app/(pages)/kontakt/page.tsx
+++ src/app/(pages)/kontakt/page.tsx
@@ -1,7 +1,14 @@
-import React from "react"
+import React from "react";
+import classNames from "classnames";
+import classes from '../../page.module.css';
+import SubPageEntry from "@/app/components/subPageEntry/subPageEntry";
 
 export default function ContactPage() {
     return(
-        <>kontakt</>
+        <div>
+            <div className={classes.subpageTopMargin}></div>            
+            <SubPageEntry/>
+        </div>
     )
 }
+{/* <div className={classNames({[classes.subpageUnitHeight]:true, ['bg-black']:true})}></div> */}
