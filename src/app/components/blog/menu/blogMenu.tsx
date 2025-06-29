802c4d77965dccc43fe20fcc513fb207a84ba9ee
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-06-29 23:21:34 +0200

    styling and hovers in blog menu

--- src/app/components/blog/menu/blogMenu.tsx
+++ src/app/components/blog/menu/blogMenu.tsx
@@ -11,7 +11,7 @@ export default async function BlogMenu() {
 
     return (
         <div className={classNames({[classes.blogMenuContainer]: true, ['h-[100vh] flex items-center fixed']:true})}>
-            <aside className={'border-l border-black p-[1vw]'}>
+            <aside className={'border-l border-black p-[10px]'}>
                 <nav className='h-full flex flex-col gap-[2vw]'>
                     <h2 className='text-3xl font-bold italic'>Kategoria</h2>
                     {categories?.map((category, index) => {
@@ -22,12 +22,15 @@ export default async function BlogMenu() {
                             <div key={index} className='flex flex-col gap-[1vw] ml-[10px]'>
                                 <h3 className='text-2xl font-bold'>{category?.name}</h3>                        
                                 <div className='flex flex-col gap-[1vw] ml-[10px]'>
-                                    {category?.posts?.map((post, index) => {                                        
-                                        return(
-                                            <Link key={index} href={'/blog/' + post?.slug}>
-                                                {post?.title}
-                                            </Link>                                                     
-                                        )
+                                    {category?.posts?.map((post, index) => {    
+                                        if(index < 2) {
+                                            return(
+                                                <Link className='relative group hover:text-white transition-colors duration-500' key={index} href={'/blog/' + post?.slug}>
+                                                    {post?.title}
+                                                    <span className='group-hover:bg-black absolute z-[-10] left-[-30px] top-[50%] transform translate-y-[-50%] w-200 h-[130%] bg-white transition-colors duration-500'></span>
+                                                </Link>                                                     
+                                            )
+                                        }                                    
                                     })}
                                 </div>
                             </div>
@@ -37,7 +40,7 @@ export default async function BlogMenu() {
                     <div className='w-1/4 flex gap-1 flex-wrap w-full'>                        
                         {tags?.map((tag, index) => {
                             return(
-                                <Link className='border border-black p-2 rounded-md' href={'/blog/' + tag.slug}>
+                                <Link key={index} className='border border-black p-1 rounded-md hover:bg-black hover:text-white transition-colors duration-500' href={'/blog/' + tag.slug}>
                                     {tag.name}
                                 </Link>            
                             )
