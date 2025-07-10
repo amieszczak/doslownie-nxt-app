16936f52c5f2f355cf90f1208dc4e9297b3f590f
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-07-10 21:39:45 +0200

    landingPageOffer link

--- src/app/components/landingPage/offer/landingPageOffer.tsx
+++ src/app/components/landingPage/offer/landingPageOffer.tsx
@@ -13,12 +13,11 @@ export default function LandingPageOffer() {
 
     const [offerId, setOfferId] = useState(1);
 
-
-    const OFFER_CONTENT : Array<{id: number, title : string, description : string, photoSrc : string}> = [
-        {id: 1, title: 'Konsultacja logopedyczna', description: 'Konsultacja logopedyczna to spotkanie w celu skontrolowania mowy dziecka i zadania pytań terapeucie odnośnie jego rozwoju. Zostaje na niej przeprowadzony wywiad z rodzicem lub opiekunem oraz badanie dziecka. Jest to pierwszy krok przed rozpoczęciem terapii, który umożliwi wspólne omówienie dalszych działań.', photoSrc: '/offer/Konsultacja_theme.png'},
-        {id: 2, title: 'Terapia logopedyczna', description: 'Terapia logopedyczna to zespół działań podejmowanych przez logopedów, dla których nadrzędnym zadaniem jest osiągnięcie zamierzonych celów logopedycznych. Czas terapii logopedycznej jest uzależniony od typu trudności oraz indywidualnej motywacji pacjenta. Pracujemy w oparciu o metody niedyrektywne, w zabawie i w relacji, z uwzględnieniem indywidualnych potrzeb każdej osoby. Terapia logopedyczna prowadzona jest z wykorzystaniem terapii miofunkcjonalnej.', photoSrc: '/offer/Terapia_theme.png'},
-        {id: 3, title: 'Trening umiejętności społecznych', description: 'Trening Umiejętności Społecznej to zajęcia ogólnorozwojowe prowadzone w grupach kilkuosobowych. Udział dziecka w zajęciach poprzedzony jest diagnozą funkcjonowania emocjonalno-społecznego dziecka i wywiadem z Rodzicami. Zajęcia TUS mają na celu rozwinięcie kompetencji społecznych w tym rozpoznawania i reagowania na swoje i cudze emocje, dobrej komunikacji czy współpracy w grupie.', photoSrc: '/offer/Tus_theme.png'},
-        {id: 4, title: 'Terapia ręki', description: 'Terapia Ręki ma na celu usprawnianie tzw. małej motoryki, czyli precyzyjnych ruchów dłoni i palców, jak również dostarczania wrażeń dotykowych i poznawania dzięki nim różnych kształtów, struktur materiałów oraz nabywanie umiejętności ich rozróżniania. Przede wszystkim służy jednak do osiągnięcia samodzielności w podstawowych czynnościach życia codziennego.', photoSrc: '/offer/Terapia_reki_theme.png'},
+    const OFFER_CONTENT : Array<{id: number, title : string, description : string, photoSrc : string, href: string}> = [
+        {id: 1, title: 'Konsultacja logopedyczna', description: 'Konsultacja logopedyczna to spotkanie w celu skontrolowania mowy dziecka i zadania pytań terapeucie odnośnie jego rozwoju. Zostaje na niej przeprowadzony wywiad z rodzicem lub opiekunem oraz badanie dziecka. Jest to pierwszy krok przed rozpoczęciem terapii, który umożliwi wspólne omówienie dalszych działań.', photoSrc: '/offer/Konsultacja_theme.png', href: '/oferta/konsultacja-logopedyczna'},
+        {id: 2, title: 'Terapia logopedyczna', description: 'Terapia logopedyczna to zespół działań podejmowanych przez logopedów, dla których nadrzędnym zadaniem jest osiągnięcie zamierzonych celów logopedycznych. Czas terapii logopedycznej jest uzależniony od typu trudności oraz indywidualnej motywacji pacjenta. Pracujemy w oparciu o metody niedyrektywne, w zabawie i w relacji, z uwzględnieniem indywidualnych potrzeb każdej osoby. Terapia logopedyczna prowadzona jest z wykorzystaniem terapii miofunkcjonalnej.', photoSrc: '/offer/Terapia_theme.png', href: '/oferta/terapia-logopedyczna' },
+        {id: 3, title: 'Trening umiejętności społecznych', description: 'Trening Umiejętności Społecznej to zajęcia ogólnorozwojowe prowadzone w grupach kilkuosobowych. Udział dziecka w zajęciach poprzedzony jest diagnozą funkcjonowania emocjonalno-społecznego dziecka i wywiadem z Rodzicami. Zajęcia TUS mają na celu rozwinięcie kompetencji społecznych w tym rozpoznawania i reagowania na swoje i cudze emocje, dobrej komunikacji czy współpracy w grupie.', photoSrc: '/offer/Tus_theme.png', href: '/oferta/trening-umiejetnosci-spolecznych'},
+        {id: 4, title: 'Terapia ręki', description: 'Terapia Ręki ma na celu usprawnianie tzw. małej motoryki, czyli precyzyjnych ruchów dłoni i palców, jak również dostarczania wrażeń dotykowych i poznawania dzięki nim różnych kształtów, struktur materiałów oraz nabywanie umiejętności ich rozróżniania. Przede wszystkim służy jednak do osiągnięcia samodzielności w podstawowych czynnościach życia codziennego.', photoSrc: '/offer/Terapia_reki_theme.png', href: '/oferta/terapia-reki'},
     ]
 
     const currentElement = OFFER_CONTENT.find(item => item.id == offerId);
@@ -31,7 +30,7 @@ export default function LandingPageOffer() {
                     <div className="flex flex-col px-25 gap-15">
                         <h2 className="text-4xl font-thin italic">{currentElement && currentElement.title}</h2>               
                         <p >{currentElement && currentElement.description}</p>
-                        <LandingPageButton href={'/oferta'} title={"dowiedz się więcej"} variant={'right'}/>
+                        <LandingPageButton href={OFFER_CONTENT.find(item => item.id == offerId)?.href ?? '#'} title={"dowiedz się więcej"} variant={'right'}/>
                     </div>            
                 </div>
                 <div className="w-1/2 overflow-hidden">
