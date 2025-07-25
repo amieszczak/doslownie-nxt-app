db824d11f1f0f66c19fd7c17a9b69ac98ccf0da4
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-07-25 09:47:47 +0200

    removed imports

--- src/app/components/subPageEntry/subPageEntry.tsx
+++ src/app/components/subPageEntry/subPageEntry.tsx
@@ -1,12 +1,4 @@
 'use client';
-import classes from '../../page.module.css';
-import styles from './subpageEntry.module.css';
-import classNames from 'classnames';
-import Image from 'next/image';
-import Link from 'next/link';
-import location from '../../../../public/contact/location.svg';
-import message from '../../../../public/contact/message.svg';
-import phone from '../../../../public/contact/phone.svg';
 import dynamic from 'next/dynamic';
 
 export default function SubPageEntry() {
