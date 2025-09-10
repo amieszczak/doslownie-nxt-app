7627e74a5396108bc8cf41c152cd035060a1a8e9
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-09-10 20:22:31 +0200

    landingPageTitleRWD

--- src/app/components/landingPage/video/landingPageVideo.tsx
+++ src/app/components/landingPage/video/landingPageVideo.tsx
@@ -26,15 +26,14 @@ export default function LandingPageVideo() {
     const dimensionsInitial = 'max-h-[1080px] max-w-[1920px] h-screen w-screen'; 
     const dimensionsScrolled = 'max-w-[1920px] h-screen w-screen'
 
-
     return(
-        <div className={classNames({[dimensionsInitial]: !componentFullScreen, [dimensionsScrolled]: componentFullScreen,['relative']: true})}>
+        <div className={classNames({[dimensionsInitial]: !componentFullScreen, [dimensionsScrolled]: componentFullScreen, ['relative']: true})}>
             <div className={classNames({['fixed z-[-1]']: true, [dimensionsInitial]: !componentFullScreen, [dimensionsScrolled]: componentFullScreen})}>
                 <Image alt="animacja tytułowa" className="w-full h-full object-cover" src={video}/>
                 <div className={classNames({["absolute bottom-0 text-white"]: true, [styles.hide]: !titleVisible, [styles.show]: titleVisible})}>
-                    <div className='relative border-t-2 border-white h-40 w-[84vw]'>
-                        <h1 className="text-[15vw] absolute bottom-0 left-[-1vw] leading-[10vw]">DOSŁOWNIE</h1> 
-                        <h2 className="text-[5vw] absolute right-0 translate-y-[-100%]">NEUROLOGOPEDIA</h2>
+                    <div className={classNames({['relative border-t-2 border-white w-[84vw]']: true, [styles.titleContainer]: true})}>                       
+                        <h1 className={styles.title}>DOSŁOWNIE</h1> 
+                        <h2 className={styles.subtitle}>NEUROLOGOPEDIA</h2>
                     </div>  
                 </div>
             </div>
