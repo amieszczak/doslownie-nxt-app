83e622396995a0c845b157854048246513adbcb0
ADRIAN MIESZCZAK <mieszczak22@gmail.com>
2025-09-14 18:19:51 +0200

    landingPageDescription rwd

--- src/app/components/landingPage/common/description/landingPageDescription.tsx
+++ src/app/components/landingPage/common/description/landingPageDescription.tsx
@@ -7,9 +7,9 @@ export default function LandingPageDescription({ title, description, href, butto
     return(
         <div className="w-1/2 flex items-center">
         <Markup variant={'left'} title={'oferta'} top={true} />
-        <div className="flex flex-col px-25 gap-15">
-            <h2 className="text-base 5xl:text-4xl font-thin italic">{title}</h2>
-            <p>{description}</p>
+        <div className="flex flex-col px-5 sm:px-10 md:px-15 lg:px-20 2xl:px-25 gap-5 xl:gap-10 2xl:gap-15">
+            <h2 className="text-base sm:text-lg md:text-xl lg:text-2xl 5xl:text-4xl font-thin italic">{title}</h2>
+            <p className='text-xs md:text-sm xl:text-base'>{description}</p>
             <LandingPageButton href={href ?? '#'} title={buttonTitle ?? 'DOWIEDZ SIĘ WIĘCEJ'} variant={variant} />
         </div>
     </div>
